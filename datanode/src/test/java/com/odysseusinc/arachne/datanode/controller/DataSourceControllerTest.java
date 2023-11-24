7deae48d2b6a29684c364f6867380bad654e8cc7
dmitrys <dmitrys-odysseus@github.com>
2023-11-24 21:44:00 +0100

    Upgrade spring-boot 2.5.14 -> 2.7.18

--- datanode/src/test/java/com/odysseusinc/arachne/datanode/controller/DataSourceControllerTest.java
+++ datanode/src/test/java/com/odysseusinc/arachne/datanode/controller/DataSourceControllerTest.java
@@ -23,7 +23,7 @@ import java.security.Principal;
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
