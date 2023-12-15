afa3ad06477a1b9b784df97bb7263fb651380580
dmitrys <dmitrys-odysseus@github.com>
2023-12-15 20:33:15 +0100

    Cleanup various leftovers of achilles and atlas integration

--- datanode/src/main/java/com/odysseusinc/arachne/datanode/service/CohortNamePatcher.java
+++
@@ -1,9 +0,0 @@
-package com.odysseusinc.arachne.datanode.service;
-
-import com.fasterxml.jackson.databind.JsonNode;
-
-public interface CohortNamePatcher {
-
-    JsonNode patchAnalysis(JsonNode analysis);
-
-}
