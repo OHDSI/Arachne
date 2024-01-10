ee623989bb0fed221794be470a6e7476135b36b1
Konstantin Yaroshovets <konstantin.yaroshovets@odysseusinc.com>
2024-01-10 16:59:00 +0200

    Docker compose fixes

--- install/docker/fix..sql
+++
@@ -1,1 +0,0 @@
-UPDATE users SET password='$2a$10$JrltmCF6zqvfdpZTYOTM0uByU1Cx9C3X0x0iRQFqlXX3bTOEsWJae';
---
+++ install/docker/install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Create necessary Folders
+folders=(
+  "~/ArachneDatanode"
+)
+
+for folder in "${folders[@]}"; do
+  if [ ! -d "$folder" ]; then
+    mkdir -p "$folder"
+    echo "Folder $folder was created."
+  else
+    echo "Folder $folder already exists. Skipping creation."
+  fi
+done
+
+
+cp datanode-example.env ~/ArachneDatanode/datanode.env
+cd ~/ArachneDatanode
+wget https://storage.googleapis.com/arachne-datanode/descriptor_base.json
+
+echo "Script completed."
