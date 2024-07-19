501a8891647447cb0d3fb1e586c5326be4440e00
dmitrys <dmitrys-odysseus@github.com>
2024-07-19 18:45:16 +0200

    AdminController cleanup

--- datanode/src/main/java/com/odysseusinc/arachne/datanode/controller/admin/AdminController.java
+++ datanode/src/main/java/com/odysseusinc/arachne/datanode/controller/admin/AdminController.java
@@ -16,20 +16,14 @@
 package com.odysseusinc.arachne.datanode.controller.admin;
 
 import com.odysseusinc.arachne.commons.api.v1.dto.util.JsonResult;
-import com.odysseusinc.arachne.datanode.Constants;
 import com.odysseusinc.arachne.datanode.controller.BaseController;
 import com.odysseusinc.arachne.datanode.dto.converters.AnalysisToSubmissionDTOConverter;
 import com.odysseusinc.arachne.datanode.dto.submission.SubmissionDTO;
 import com.odysseusinc.arachne.datanode.dto.user.UserDTO;
-import com.odysseusinc.arachne.datanode.exception.AuthException;
 import com.odysseusinc.arachne.datanode.exception.PermissionDeniedException;
 import com.odysseusinc.arachne.datanode.model.analysis.Analysis;
 import com.odysseusinc.arachne.datanode.model.user.User;
 import com.odysseusinc.arachne.datanode.repository.AnalysisRepository;
-import com.odysseusinc.arachne.datanode.service.AnalysisService;
-import com.odysseusinc.arachne.datanode.service.DataNodeService;
-import com.odysseusinc.arachne.datanode.service.ExecutionEngineIntegrationService;
-import com.odysseusinc.arachne.datanode.service.ExecutionEngineStatus;
 import com.odysseusinc.arachne.datanode.service.UserService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,7 +36,6 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -67,12 +60,6 @@ public class AdminController extends BaseController {
     private  GenericConversionService conversionService;
     @Autowired
     private  AnalysisRepository analysisRepository;
-    @Autowired
-    private  AnalysisService analysisService;
-    @Autowired
-    private  DataNodeService dataNodeService;
-    @Autowired
-    private  ExecutionEngineIntegrationService executionEngineIntegrationService;
 
     public AdminController(UserService userService) {
         super(userService);
@@ -125,28 +112,12 @@ public class AdminController extends BaseController {
         return new JsonResult<>(JsonResult.ErrorCode.NO_ERROR);
     }
 
-    @ApiOperation(value = "Invalidate all unfinished analyses")
-    @PostMapping(Constants.Api.Analysis.INVALIDATE_ALL_UNFINISHED)
-    public Integer invalidateAllUnfinishedAnalyses(final Principal principal) throws PermissionDeniedException {
-
-        if (principal == null) {
-            throw new AuthException("user not found");
-        }
-
-        return analysisService.invalidateAllUnfinishedAnalyses(getUser(principal));
-    }
-
     @ApiOperation(value = "list submissions")
     @GetMapping("/api/v1/admin/submissions")
     public Page<SubmissionDTO> list(@PageableDefault(value = DEFAULT_PAGE_SIZE, sort = "id",
             direction = Sort.Direction.DESC) Pageable pageable) {
 
-        Pageable p;
-        if (!isCustomSort(pageable)) {
-            p = pageable;
-        } else {
-            p = buildPageRequest(pageable);
-        }
+        Pageable p = isCustomSort(pageable) ? buildPageRequest(pageable) : pageable;
         Page<Analysis> analyses;
         if (isFinishedSort(pageable)) {
             analyses = analysisRepository.findAllPagedOrderByFinished(p);
@@ -160,18 +131,7 @@ public class AdminController extends BaseController {
         return analyses.map(analysis -> analysisToSubmissionDTO.convert(analysis));
     }
 
-    @ApiOperation(value = "get execution engine status")
-    @GetMapping("/api/v1/admin/execution-engine/status")
-    public EngineStatusResponse getExecutionEngineStatus() {
-        return new EngineStatusResponse(executionEngineIntegrationService.getExecutionEngineStatus());
-    }
-
     protected Pageable buildPageRequest(Pageable pageable) {
-
-        if (pageable.getSort() == null) {
-            return pageable;
-        }
-        PageRequest result;
         List<String> properties = new LinkedList<>();
         Sort.Direction direction = Sort.Direction.ASC;
         for (Sort.Order order : pageable.getSort()) {
@@ -183,10 +143,9 @@ public class AdminController extends BaseController {
                 properties.add(property);
             }
         }
-        result = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), direction,
-                properties.toArray(new String[properties.size()]));
 
-        return result;
+        return PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), direction,
+                properties.toArray(new String[properties.size()]));
     }
 
     private boolean isCustomSort(final Pageable pageable) {
@@ -237,10 +196,4 @@ public class AdminController extends BaseController {
         propertiesMap.put("status", p -> p.add("journal.state"));
     }
 
-    private class EngineStatusResponse {
-        public EngineStatusResponse(final ExecutionEngineStatus status) {
-            this.status = status;
-        }
-        public ExecutionEngineStatus status;
-    }
 }
