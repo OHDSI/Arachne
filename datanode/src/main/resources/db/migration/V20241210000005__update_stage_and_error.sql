b8ab0b09063e85f06e61fb571ac72c4c684ce2d7
ymolodkov <yaroslav_molodkov@epam.com>
2025-01-10 19:19:27 +0100

    Wrong type for the analysis_state_journal.error

--- datanode/src/main/resources/db/migration/V20241210000005__update_stage_and_error.sql
+++ datanode/src/main/resources/db/migration/V20241210000005__update_stage_and_error.sql
@@ -1,4 +1,4 @@
 UPDATE analysis_state_journal SET
                                   stage = analyses.stage,
                                   error = analyses.error
-FROM analyses WHERE analyses.current_state_id = analyses.id;
+FROM analyses WHERE analyses.current_state_id = analysis_state_journal.id;
